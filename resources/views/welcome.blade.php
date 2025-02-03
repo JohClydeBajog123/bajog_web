e56ba173c1384cabcee88df0662a22709bacda02
John Clyde <johnclydebajog123@gmail.com>
2025-02-03 14:17:34 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Landing Page</title>
+  <link rel="stylesheet" href="../../../landing-form/styles.css">
+</head>
+<body>
+  <header>
+    <div class="header-content">
+      <nav class="auth-links">
+        <a href="{{ route('login')}}" class="auth-btn">Log In</a>
+        <a href="{{ route('register')}}" class="auth-btn">Sign Up</a>
+      </nav>
+    </div>
+  </header>
+
+  <main>
+    <section class="hero">
+      <h1>JC Appareal</h1>
+      <p>Welcome to JC's Appareal.</p>
+    </section>
+  </main>
+</body>
+</html>
